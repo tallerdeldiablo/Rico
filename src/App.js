953bab9113115e7c54ea8241704a6ab462712922
rico <rikardoan@gmail.com>
2022-01-26 15:33:34 -0500

    footer fixed-bottom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React from "react";
+import Foot from './components/Foot';
 import PortfolioContainer from "./components/PortfolioContainer";
 
-const App = () => <PortfolioContainer />;
+// const App = () => <PortfolioContainer />;
 
+
+
+function App() {
+  return (
+    <div >
+      <PortfolioContainer />
+      <Foot />
+    </div>
+  );
+}
 export default App;
