027bf51fcb6b5f281f402faeb529945537b431e5
rico <rikardoan@gmail.com>
2022-01-27 18:30:18 -0500

    updated links multimedia page

--- src/components/pages/Coding.js
+++ src/components/pages/Coding.js
@@ -5,80 +5,82 @@ export default function Coding() {
     <>
 <div class="row row-cols-2 row-cols-md-3 g-12">
   <div class="col">
-    <div class="card">
+    <div class="card"> <a href='https://kelseykodes.github.io/Gymder/'>
     <img
                 src="https://tallerdeldiablo.github.io/My-Portfolio/assets/img/project-gym.jpg"
                 class="card-img-top"
                 alt="Mywork"
-              />
+              /></a>
+
+
       <div class="card-body">
         <h5 class="card-title">Gymder</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text">This app was a collaborative project designed for users who exercise in the Atlanta Metropolitan area.</p>
       </div>
     </div>
   </div>
   <div class="col">
-    <div class="card">
+    <div class="card"> <a href='https://tallerdeldiablo.github.io/Quick-Weather-Finder/'>
     <img
                 src="https://tallerdeldiablo.github.io/My-Portfolio/assets/img/weather.jpg"
                 class="card-img-top"
                 alt="Mywork"
-              />
+              /></a>
       <div class="card-body">
         <h5 class="card-title">Weather Finder</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text">The intention of this app is to allow users to search for the weather forecast in any city of their choice, using a weather API.</p>
       </div>
     </div>
   </div>
   <div class="col">
-    <div class="card">
+    <div class="card"> <a href='https://todologo.co'>
      <img
                 src="https://tallerdeldiablo.github.io/My-Portfolio/assets/img/todolo.png"
                 class="card-img-top"
                 alt="Mywork"
-              />
+              /></a>
       <div class="card-body">
-        <h5 class="card-title">Gymder</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+        <h5 class="card-title">Todologos</h5>
+        <p class="card-text">This is a yellow pages-style website created with WordPress to search for and promote local businesses.</p>
       </div>
     </div>
   </div>
   <div class="col">
-    <div class="card">
+    <div class="card"> <a href='https://mysterious-earth-01404.herokuapp.com/'>
      <img
                 src="https://tallerdeldiablo.github.io/My-Portfolio/assets/img/weather.jpg"
                 class="card-img-top"
                 alt="Mywork"
-              />
+              /></a>
       <div class="card-body">
         <h5 class="card-title">TKNO</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text">This is a blog similar to Wordpress, where the user can log in and make a post and comments or read others' posts when not logged in.</p>
       </div>
     </div>
   </div>
   <div class="col">
-    <div class="card">
+    <div class="card"> <a href='https://stormy-retreat-41343.herokuapp.com'>
      <img
-                src="https://tallerdeldiablo.github.io/My-Portfolio/assets/img/project-gym.jpg"
+                src="https://tallerdeldiablo.github.io/My-Portfolio/assets/img/notetaker.png"
                 class="card-img-top"
                 alt="Mywork"
-              />
+              /></a>
       <div class="card-body">
         <h5 class="card-title">Note Taker</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text">This is an app designed to store and delete notes.</p>
       </div>
     </div>
   </div>
   <div class="col">
-    <div class="card">
+    <div class="card"> <a href='https://tallerdeldiablo.github.io/Workday-planner/'>
      <img
                 src="https://user-images.githubusercontent.com/57916204/135383781-576245c8-62ad-4e8e-b911-4ee009c142c3.png"
                 class="card-img-top"
                 alt="Mywork"
-              />
+              /></a>
       <div class="card-body">
         <h5 class="card-title">Day Planner</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text">This is an </p>
       </div>
     </div>
   </div>
--- src/components/pages/Multimedia.js
+++ src/components/pages/Multimedia.js
@@ -5,13 +5,12 @@ export default function Multimedia() {
     <>
 <div class="row row-cols-2 row-cols-md-3 g-12">
   <div class="col">
-    <div class="card">
-     
-    <img
-                src=".https://tallerdeldiablo.github.io/My-Portfolio/assets/img/alas.png"
+    <div class="card"> <a href='https://vimeo.com/546049470'>
+         <img 
+                src="https://tallerdeldiablo.github.io/My-Portfolio/assets/img/alas.png"
                 class="card-img-top"
                 alt="Mywork"
-              />
+              /> </a>
       <div class="card-body">
         <h5 class="card-title">CAD/ Rendering</h5>
         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -19,12 +18,12 @@ export default function Multimedia() {
     </div>
   </div>
   <div class="col">
-    <div class="card">
-    <img
-                src="https://tallerdeldiablo.github.io/My-Portfolio/assets/img/2d.png"
+    <div  class="card"> <a href='https://www.youtube.com/watch?v=ALnWLpwidg4'>
+    <img href='https://ricardoangulo.myportfolio.com/'
+                src="https://tallerdeldiablo.github.io/My-Portfolio/assets/img/2danim.png"
                 class="card-img-top"
                 alt="Mywork"
-              />
+              /></a>
       <div class="card-body">
         <h5 class="card-title">2d animation</h5>
         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -32,57 +31,19 @@ export default function Multimedia() {
     </div>
   </div>
   <div class="col">
-    <div class="card">
+    <div class="card">  <a href='https://ricardoangulo.myportfolio.com/'>
      <img
                 src="https://tallerdeldiablo.github.io/My-Portfolio/assets/img/photo.png"
                 class="card-img-top"
                 alt="Mywork"
-              />
-      <div class="card-body">
-        <h5 class="card-title">3d modeling</h5>
+              /></a>
+      <div class="card-body"> 
+        <h5 class="card-title">Photography</h5>
         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
       </div>
     </div>
   </div>
-  <div class="col">
-    <div class="card">
-     <img
-                src="https://tallerdeldiablo.github.io/My-Portfolio/assets/img/weather.jpg"
-                class="card-img-top"
-                alt="Mywork"
-              />
-      <div class="card-body">
-        <h5 class="card-title">TKNO</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card">
-     <img
-                src="https://tallerdeldiablo.github.io/My-Portfolio/assets/img/project-gym.jpg"
-                class="card-img-top"
-                alt="Mywork"
-              />
-      <div class="card-body">
-        <h5 class="card-title">Note Taker</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card">
-     <img
-                src="https://user-images.githubusercontent.com/57916204/135383781-576245c8-62ad-4e8e-b911-4ee009c142c3.png"
-                class="card-img-top"
-                alt="Mywork"
-              />
-      <div class="card-body">
-        <h5 class="card-title">Day Planner</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      </div>
-    </div>
-  </div>
+
 </div>
 
 
